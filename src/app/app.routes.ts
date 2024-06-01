1a53ce6caf01d2c13b0960697f27d0f0c20883d8
AdelisJose97 <adelisjose1997@gmail.com>
2024-05-31 21:02:37 -0400

    fix: redirect to login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { TasksComponent } from "./modules/tasks/tasks.component";
 export const routes: Routes = [
     {
         path: "",
-        redirectTo: "/home",
+        redirectTo: "/login",
         pathMatch: "full"
     },
     {
