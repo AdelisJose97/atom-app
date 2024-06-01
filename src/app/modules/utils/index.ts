ec122af3d4cd38911617c061918b9cacd1af4a2c
AdelisJose97 <adelisjose1997@gmail.com>
2024-05-31 20:12:01 -0400

    use api deployed url

--- src/app/modules/utils/index.ts
+++ src/app/modules/utils/index.ts
@@ -1,28 +1,28 @@
 import { HttpErrorResponse } from "@angular/common/http";
 import { throwError } from "rxjs";
 
-export const apiUrl = "http://localhost:3000";
+export const apiUrl = "https://atom-api-drab.vercel.app";
 
 export const CLOSE_DIALOG_STATUS = "close";
 export const CONFIRM_DIALOG_STATUS = "create";
 export const EDIT_TASK_DIALOG_STATUS = "edit";
 
 export interface DialogResponse<T> {
-    action: string;
+    action: string
     data?: T
 }
 
 export interface ChangeStatusTask<T> {
     status: boolean
-    data?:T;
+    data?: T
 }
 
 export function handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
-        // eslint-disable-next-line no-console
+    // eslint-disable-next-line no-console
         console.error("An error occurred:", error.error);
     } else {
-        // eslint-disable-next-line no-console
+    // eslint-disable-next-line no-console
         console.error(
             `Backend returned code ${error.status}, body was: `,
             error.error
